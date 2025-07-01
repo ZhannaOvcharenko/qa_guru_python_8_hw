14ffe9678ec9c21e83c54758ccbd544b6bc3d64a
ZhannaOvcharenko <ZOvcharenko99@gmail.com>
2025-07-02 00:40:25 +0300

    Add file models

--- models.py
+++ models.py
@@ -13,30 +13,27 @@ class Product:
         self.description = description
         self.quantity = quantity
 
+    """Метод для проверки количества продуктов"""
     def check_quantity(self, quantity) -> bool:
-        """
-        TODO Верните True если количество продукта больше или равно запрашиваемому
-            и False в обратном случае
-        """
-        raise NotImplementedError
+        if self.quantity >= quantity:
+            return True
+        else:
+            return False
 
+    """Метод покупки"""
     def buy(self, quantity):
-        """
-        TODO реализуйте метод покупки
-            Проверьте количество продукта используя метод check_quantity
-            Если продуктов не хватает, то выбросите исключение ValueError
-        """
-        raise NotImplementedError
+        if self.check_quantity(quantity):
+            self.quantity -= quantity
+        else:
+            raise ValueError("Недостаточно продуктов")
 
+    """Метод для возврата хеша продукта по его наименованию и описанию"""
     def __hash__(self):
         return hash(self.name + self.description)
 
 
 class Cart:
-    """
-    Класс корзины. В нем хранятся продукты, которые пользователь хочет купить.
-    TODO реализуйте все методы класса
-    """
+    """Класс корзины. В нем хранятся продукты, которые пользователь хочет купить."""
 
     # Словарь продуктов и их количество в корзине
     products: dict[Product, int]
@@ -46,30 +43,42 @@ class Cart:
         self.products = {}
 
     def add_product(self, product: Product, buy_count=1):
-        """
-        Метод добавления продукта в корзину.
-        Если продукт уже есть в корзине, то увеличиваем количество
-        """
-        raise NotImplementedError
+        """Метод добавления продукта в корзину.
+        Если продукт уже есть в корзине, то увеличиваем количество"""
+        if product in self.products:
+            self.products[product] += buy_count
+        else:
+            self.products[product] = buy_count
 
     def remove_product(self, product: Product, remove_count=None):
-        """
-        Метод удаления продукта из корзины.
+        """Метод удаления продукта из корзины.
         Если remove_count не передан, то удаляется вся позиция
-        Если remove_count больше, чем количество продуктов в позиции, то удаляется вся позиция
-        """
-        raise NotImplementedError
+        Если remove_count больше, чем количество продуктов в позиции, то удаляется вся позиция"""
+        if product in self.products:
+            if remove_count is None or self.products[product] <= remove_count:
+                del self.products[product]
+            else:
+                self.products[product] -= remove_count
 
+    """Метод для очистки корзины"""
     def clear(self):
-        raise NotImplementedError
+        self.products.clear()
 
+    """Метод для расчета стоимости всех товаров в корзине"""
     def get_total_price(self) -> float:
-        raise NotImplementedError
+        total_price = 0.0
+        for product, count in self.products.items():
+            total_price += product.price * count
+        return total_price
 
     def buy(self):
-        """
-        Метод покупки.
+        """Метод покупки.
         Учтите, что товаров может не хватать на складе.
-        В этом случае нужно выбросить исключение ValueError
-        """
-        raise NotImplementedError
+        В этом случае нужно выбросить исключение ValueError"""
+        for product, quantity in self.products.items():
+            if not product.check_quantity(quantity):
+                raise ValueError
+        for product, quantity in self.products.items():
+            product.buy(quantity)
+        self.clear()
+
